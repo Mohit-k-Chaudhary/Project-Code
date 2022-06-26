9a65032205e04feefe7e796c5958443106421840
Mohit-k-Chaudhary <mohit.chaudhary.freelancer@gmail.com>
2022-06-26 21:05:28 +0530

    26-06 Update

--- calculations.php
+++ calculations.php
@@ -1,12 +1,6 @@
 
 <?php
 
-$name = $_POST['firstname'];
-$code_number = $_POST['code-number'];
-$Valuation_Date = $_POST['Valuation-Date'];
-$country = $_POST['country'];
-
-
-echo "your name is: ",$name;
+echo "Backend Calculations is Under Constructions: ",$name;
 
 ?>
